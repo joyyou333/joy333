cfffcd0c382235014fb2ae2ec28d87c8031e448d
joy <joy.you@baifu-tech.net>
2021-09-13 19:03:06 +0800

    two

--- hello.go
+++ hello.go
@@ -1,8 +1,11 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	fmt.Println("Hello Golang")
 }
+
 // 撰寫程式 > 建置(build) > 執行程式
-// 建置: go build 程式檔案的名稱 
-//
+// 建置: go build 程式檔案的名稱
+//123
